4cc94a7fb32d924721d95b8bc937ea7fb64ba2ee
mark390 <mark.ross@ampsight.com>
2021-03-14 18:50:31 -0400

    fixed missing ;

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
     $dsn = 'mysql:host=eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=sx8o31qj3imbsc4z';
     $username = 'vi07uqbw6iakj2kg';
-    $password = 'vtim3w5bc6tpch8f'
+    $password = 'vtim3w5bc6tpch8f';
 
     try {
         $db = new PDO($dsn, $username, $password);
