98d8895b769394a1747d1ee3980f5a6ad61e7d56
mark390 <55713492+mark390@users.noreply.github.com>
2021-04-11 22:34:11 -0400

    Update thankyou.php

--- view/thankyou.php
+++ view/thankyou.php
@@ -1,11 +1,7 @@
 <?php include('view/header.php'); ?>
 
     <h1>Thank you for registering <?php echo $_SESSION['userid']; ?></h1>
-<<<<<<< Updated upstream
-    <a href="../index.php">Return to Homepage</a>
-=======
     <a href=".">Return to Homepage</a>
->>>>>>> Stashed changes
 
 <?php include('view/footer.php'); ?>
 
