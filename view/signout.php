35477754d9e4d12982662dfb157052a9922fc1b3
mark390 <55713492+mark390@users.noreply.github.com>
2021-04-11 22:32:36 -0400

    Update signout.php

--- view/signout.php
+++ view/signout.php
@@ -4,11 +4,6 @@ $_SESSION = array();
 session_destroy();
 ?>
 <h1>You have been signed out!</h1><br>
-<<<<<<< Updated upstream
-<a href="./index.php?action=list_vehicles">Return to Homepage</a>
-=======
 <a href="../index.php">Return to Homepage</a>
->>>>>>> Stashed changes
-
 
 <?php include('view/footer.php'); ?>
